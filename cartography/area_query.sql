3633f8a97235cf66885782dad843c9995119cbf6
darsvid <d.svidzinska@gmail.com>
2018-10-18 13:17:55 +0300

    update area sql-query

--- cartography/area_query.sql
+++ cartography/area_query.sql
@@ -1,3 +1,2 @@
-SELECT ST_Area(
-    ST_GeographyFromText(ST_AsText(geom)))/10000
-        AS area_ha FROM "table"
+ALTER TABLE  "your_table" ADD COLUMN area_ha double precision;
+UPDATE "your_table" SET area_ha = ROUND((ST_AREA(geom::geography)/10000)::numeric, 3);
